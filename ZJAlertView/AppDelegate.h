3588888307b16c4b20455543f4d0baa852843bc5
Project02 <343438569@qq.com>
2017-01-05 17:52:13 +0800

    Initial Commit

---
+++ ZJAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJAlertView
+//
+//  Created by mac on 17/1/5.
+//  Copyright © 2017年 zhangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJAlertView
+//
+//  Created by mac on 17/1/5.
+//  Copyright © 2017年 zhangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
